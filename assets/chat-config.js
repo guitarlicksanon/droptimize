9ded4a34a3c96a4463aa8bab751ea0cb29299561
Blake <hoa@Hoas-Mac-mini.local>
2026-05-21 23:38:03 -0300

    Add subject selector to droptimize chat widget: Site Audits, Site Rebuilds, Pricing

--- assets/chat-config.js
+++ assets/chat-config.js
@@ -1,1 +1,1 @@
-window.OYEChat = { brand: 'droptimize', adminUrl: 'https://bink.oyecreations.com' };
+window.OYEChat = { brand: 'droptimize', adminUrl: 'https://bink.oyecreations.com', subjects: ['Site Audits', 'Site Rebuilds', 'Pricing'] };
